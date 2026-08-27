7410459763e3d7ceb24fdd1ebc405f2bc7c4de13
Ccollins101609 <Ccollins1016@gmail.com>
2026-08-27 12:38:55 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Christian Collins on 8/27/26.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
